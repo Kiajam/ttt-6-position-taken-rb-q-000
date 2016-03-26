961815dda163673f92376ec7e517f0c8eb5873b6
Kia <kiawhitehead@gmail.com>
2016-03-26 19:50:23 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,11 @@
-# code your #position_taken? method here!
+
+def position_taken?(board, position)
+  if
+    board[position] == " " ||
+    board[position] == ""  ||
+    board[position] == nil
+    false #I can return false or true or have it as last line of the method. I don't need to 'puts' or 'print'
+  else #so anything other than above should return true. So I don't have to code for X,O
+     true
+  end
+end
